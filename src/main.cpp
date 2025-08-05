1a268432241a21228209535ef80379ed81afe579
Nightcrawler <denbrylski@gmail.com>
2025-08-05 04:24:52 +0300

    apply fusion style

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
     Configuration storage;
 
     QApplication a(argc, argv);
+    a.setStyle("Fusion");
+    
     QApplication::setWindowIcon(QIcon(":/images/icon256.png"));
 
     MainWindow window(nullptr, storage);
